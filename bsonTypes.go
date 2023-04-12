927c36b1d3f77c4d2ea7281b195735b0e4e1bd32
phuonghn <phuonghn@vissoft.vn>
2023-04-12 11:31:32 +0700

    fix range filter

--- bsonTypes.go
+++ bsonTypes.go
@@ -23,7 +23,7 @@ func detectDateComparisonOperator(field string, values []string) bson.M {
 	if len(values) == 2 {
 		value := values[0]
 		if value[0:3] == "<=>" {
-			gtValue := value[4:]
+			gtValue := value[3:]
 			ltValue := values[1]
 			filter := bson.M{
 				field: bson.M{"$gte": gtValue, "$lte": ltValue},
@@ -167,7 +167,7 @@ func detectNumericComparisonOperator(field string, values []string, numericType 
 	if len(values) == 2 {
 		value := values[0]
 		if value[0:3] == "<=>" {
-			gtValue := value[4:]
+			gtValue := value[3:]
 			ltValue := values[1]
 			filter := bson.M{
 				field: bson.M{"$gte": gtValue, "$lte": ltValue},
@@ -357,7 +357,7 @@ func detectStringComparisonOperator(field string, values []string, bsonType stri
 	if len(values) == 2 {
 		value := values[0]
 		if value[0:3] == "<=>" {
-			gtValue := value[4:]
+			gtValue := value[3:]
 			ltValue := values[1]
 			filter := bson.M{
 				field: bson.M{"$gte": gtValue, "$lte": ltValue},
